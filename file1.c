b7e2ae4accf4ca9ec055a3d3e316ecb446abd096
sirak <ksirak@gmail.com>
2017-04-27 13:58:22 +0700

    8
    8

--- file1.c
+++ file1.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 void main(){
     printf("Hello World\n");
+<<<<<<< HEAD
     printf("\n");
     printf("Sirak\n");
+=======
+>>>>>>> parent of 7920a8c... 5
     // test
     // 3rd
     //44444
