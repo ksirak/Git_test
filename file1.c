73e3cdcf00133d52e56b0a98b4422a12a45ae4e6
sirak <ksirak@gmail.com>
2017-04-27 14:20:31 +0700

    Revert "5"
    This reverts commit 7920a8c1c2d1f3de25f311db50e029992f30665d.

--- file1.c
+++ file1.c
@@ -2,6 +2,7 @@
 void main(){
     printf("Hello World\n");
 <<<<<<< HEAD
+<<<<<<< HEAD
     printf("\n");
 <<<<<<< HEAD
     printf("Sirak\n");
@@ -9,6 +10,8 @@ void main(){
 >>>>>>> parent of 7920a8c... 5
 =======
 >>>>>>> parent of 53a9911... 6
+=======
+>>>>>>> parent of 7920a8c... 5
     // test
     // 3rd
     //44444
