c47fabb42f9a8f2cb765bb3526a0ce18c89c6120
sirak <ksirak@gmail.com>
2017-04-27 14:08:01 +0700

    Revert "6"
    This reverts commit 53a9911cf182061d84360c5e4158a8c775b68a91.

--- file1.c
+++ file1.c
@@ -3,9 +3,12 @@ void main(){
     printf("Hello World\n");
 <<<<<<< HEAD
     printf("\n");
+<<<<<<< HEAD
     printf("Sirak\n");
 =======
 >>>>>>> parent of 7920a8c... 5
+=======
+>>>>>>> parent of 53a9911... 6
     // test
     // 3rd
     //44444
--- file2.c
+++ file2.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
 void main(){
     printf(" from file2 Hello World\n");
-    printf("\n");
 }
