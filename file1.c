6e96964491d695e66544d7b02c53e8fddc9c7c91
sirak <ksirak@gmail.com>
2017-04-29 20:04:00 +0700

    13
    13

--- file1.c
+++ file1.c
@@ -3,10 +3,20 @@ void main(){
     printf("Hello World\n");
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> parent of b7e2ae4... 8
+>>>>>>> parent of af5720f... 12
     printf("\n");
 <<<<<<< HEAD
 <<<<<<< HEAD
     printf("Sirak\n");
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> parent of af5720f... 12
 =======
 >>>>>>> parent of 7920a8c... 5
 =======
@@ -15,6 +25,11 @@ void main(){
 >>>>>>> parent of 7920a8c... 5
 =======
 >>>>>>> parent of 53a9911... 6
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> parent of b7e2ae4... 8
+>>>>>>> parent of af5720f... 12
     // test
     // 3rd
     //44444
