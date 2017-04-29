6a3f758f85edaa2a7a0b55493015c1184c5fb425
sirak <ksirak@gmail.com>
2017-04-29 22:43:15 +0700

    14
    14

--- file1.c
+++ file1.c
@@ -1,51 +1,10 @@
 #include<stdio.h>
 void main(){
     printf("Hello World\n");
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> parent of af5720f... 12
-=======
->>>>>>> parent of af5720f... 12
-=======
->>>>>>> parent of b7e2ae4... 8
->>>>>>> parent of af5720f... 12
+
     printf("\n");
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     printf("Sirak\n");
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> parent of af5720f... 12
-=======
->>>>>>> parent of af5720f... 12
-=======
->>>>>>> parent of 7920a8c... 5
-=======
->>>>>>> parent of 53a9911... 6
-=======
->>>>>>> parent of 7920a8c... 5
-=======
->>>>>>> parent of 53a9911... 6
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> parent of b7e2ae4... 8
-<<<<<<< HEAD
->>>>>>> parent of af5720f... 12
-=======
->>>>>>> parent of b7e2ae4... 8
->>>>>>> parent of af5720f... 12
-=======
->>>>>>> parent of af5720f... 12
-    // test
-    // 3rd
+
     //44444
 }
